e238fb4e96bf1f5a0db39f151b412bd9b2c0eed4
Lorena Diaz <ldiaz@plus54.com>
2023-07-25 19:06:45 -0300

    Demo react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+import "./App.css";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: "294.67",
+    date: new Date(2022, 1, 14),
+  },
+  {
+    id: "e2",
+    title: "Restaurants",
+    amount: "50.50",
+    date: new Date(2022, 4, 20),
+  },
+  {
+    id: "e3",
+    title: "Supermarket",
+    amount: "70.87",
+    date: new Date(2021, 3, 23),
+  },
+  {
+    id: "e4",
+    title: "Rent",
+    amount: "140.90",
+    date: new Date(2020, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = expense => {
+    // setExpenses([expense, ...expenses]);
+    // Better way to take the last snapshot of the array. The better way to update the state
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
